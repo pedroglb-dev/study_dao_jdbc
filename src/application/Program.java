386e1ca38cc6188d07d8255ab73c88a68c368e12
Pedro Gilson <pedrog.brilhante@gmail.com>
2026-09-24 16:58:26 -0300

    add Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "books");
+		
+		Seller seller = new Seller(1, "Carlos", "carlos@gmail.com", new Date(), 5000.0, obj );
 
-		IO.print(obj);
+		IO.println(seller);
 	}
 
 }
