211e58da3ac07c922a9965cf36fcc96d7211c759
Pedro Gilson <pedrog.brilhante@gmail.com>
2026-09-24 17:22:57 -0300

    add SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		Department obj = new Department(1, "books");
 		
 		Seller seller = new Seller(1, "Carlos", "carlos@gmail.com", new Date(), 5000.0, obj );
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		IO.println(seller);
 	}
